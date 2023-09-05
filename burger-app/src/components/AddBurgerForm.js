cc9f4f6794a7f737c20a3bcf4aae3e19e8d932c5
Jmillamena <jcmillamena@gmail.com>
2023-09-05 11:58:37 -1000

    creates burger form skeleton

--- burger-app/src/components/AddBurgerForm.js
+++ burger-app/src/components/AddBurgerForm.js
@@ -1,9 +1,32 @@
 import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 function AddBurgerForm() {
   return (
     <div>
-      <h3>Add A Burger</h3>
+      <Form>
+        <h3>Add A Burger</h3>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Burger Name</Form.Label>
+          <Form.Control type="burger name" placeholder="Burger of the Day" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Ingredients</Form.Label>
+          <Form.Control as="textarea" rows={3} />
+        </Form.Group>
+        <Form.Group controlId="formFile" className="mb-3">
+          <Form.Label>Add Image</Form.Label>
+          <Form.Control type="file" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Add Price</Form.Label>
+          <Form.Control type="price" placeholder="price of burger" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Add Burger
+        </Button>
+      </Form>
     </div>
   );
 }
