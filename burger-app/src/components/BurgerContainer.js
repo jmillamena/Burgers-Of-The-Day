f6106dca2569c94f8b7cd13459956b53bf091791
Jmillamena <jcmillamena@gmail.com>
2023-09-06 05:38:09 -1000

    adds fetch to burger container

--- burger-app/src/components/BurgerContainer.js
+++ burger-app/src/components/BurgerContainer.js
@@ -1,14 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search";
 import AddBurgerForm from "./AddBurgerForm";
 import BurgerList from "./BurgerList";
 
 function BurgerContainer({ searchInput, setSearchInput }) {
+  const [burgers, setBurger] = useState([]);
+
+  const addBurger = (burger) => {
+    fetch("http://localhost:3000/burgers", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(burger),
+    })
+      .then((res) => res.json())
+      .then((burger) => {
+        setBurger([...burgers, burger]);
+      });
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:3000/burgers")
+      .then((res) => res.json())
+      .then(setBurger);
+  }, []);
+
   return (
     <div>
       <Search searchInput={searchInput} setSearchInput={setSearchInput} />
-      <AddBurgerForm />
-      <BurgerList />
+      <AddBurgerForm addBurger={addBurger} />
+      <BurgerList searchInput={searchInput} burgers={burgers} />
     </div>
   );
 }
