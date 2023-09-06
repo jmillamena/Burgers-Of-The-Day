2eed8c338b3c58e7d6655628c9309c6d6a9603f0
Jazlin Yu <jazlinsub@gmail.com>
2023-09-06 11:15:46 -0700

    render data from local api

--- burger-app/src/components/BurgerList.js
+++ burger-app/src/components/BurgerList.js
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Burgers from "./Burgers";
 
 function BurgerList() {
-  return <Burgers />;
+  // const burger = {
+  //   id: 1,
+  //   date: "2/22/2015",
+  //   name: "Itsy Bitsy Teeny Weenie Yellow Polka-Dot Zucchini Burger",
+  //   ingredients: ["Yogurt Sauce", "Zucchini", "Beef Patty"],
+  //   image: "https://i.ibb.co/KVzjMFf/Screenshot-2023-09-01-at-11-31-40-AM.png",
+  //   price: 6.75,
+  // };
+  const [burgers, setBurgers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/burgers")
+      .then((r) => r.json())
+      .then((burgers) => setBurgers(burgers));
+  }, []);
+
+  const burgerCards = burgers.map((burger) => {
+    return (
+      <Burgers
+        key={burger.id}
+        name={burger.name}
+        date={burger.date}
+        image={burger.image}
+        price={burger.price}
+        ingredientsList={burger.ingredients.map((ingredient, index) => (
+          <li key={index}>{ingredient}</li>
+        ))}
+      />
+    );
+  });
+  return <ul className="cards">{burgerCards}</ul>;
 }
 
 export default BurgerList;
