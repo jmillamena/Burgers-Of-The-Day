f8ab90b74d9f3ee59d37f99c23fd8fbd2997c212
Jazlin Yu <jazlinsub@gmail.com>
2023-09-06 13:20:06 -0700

    fixfetch/cleancards

--- burger-app/src/components/Burgers.js
+++ burger-app/src/components/Burgers.js
@@ -1,30 +1,23 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 
-function Burgers({ date, name, image, price, ingredients }) {
+function Burgers({ date, name, image, price, ingredientsList }) {
   return (
     <Card style={{ width: "20rem" }}>
       <Card.Body>
         <Card.Header>
-          {date} <div></div> {price}
+          {date} <div></div> ${price}
         </Card.Header>
         <Card.Title>{name}</Card.Title>
       </Card.Body>
       <Card.Img src={image} variant="top" />
       <Card.Body>
         <Card.Title>
-          <ul>{ingredients}</ul>
+          <ul>{ingredientsList}</ul>
         </Card.Title>
       </Card.Body>
     </Card>
   );
 }
-const burger = {
-  id: 1,
-  date: "2/22/2015",
-  name: "Itsy Bitsy Teeny Weenie Yellow Polka-Dot Zucchini Burger",
-  ingredients: ["Yogurt Sauce", "Zucchini", "Beef Patty"],
-  image: "https://i.ibb.co/KVzjMFf/Screenshot-2023-09-01-at-11-31-40-AM.png",
-  price: 6.75,
-};
+
 export default Burgers;
