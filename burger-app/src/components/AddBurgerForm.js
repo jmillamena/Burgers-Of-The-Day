50d1559bec8b1ddeb4940da1fa3aa4d8e5d4f68c
Jmillamena <jcmillamena@gmail.com>
2023-09-07 14:17:53 -1000

    edit form for ingredient submission

--- burger-app/src/components/AddBurgerForm.js
+++ burger-app/src/components/AddBurgerForm.js
@@ -7,7 +7,7 @@ import "./App.css";
 function AddBurgerForm({ addBurger }) {
   const [burgerData, setBurgerData] = useState({
     name: "",
-    ingredients: "",
+    ingredients: [],
     image: "",
     price: "",
   });
@@ -17,13 +17,32 @@ function AddBurgerForm({ addBurger }) {
     setBurgerData({ ...burgerData, [name]: value });
   };
 
+  const handleIngredientChange = (e, index) => {
+    const newIngredients = [...burgerData.ingredients];
+    newIngredients[index] = e.target.value;
+    setBurgerData({ ...burgerData, ingredients: newIngredients });
+  };
+
+  const handleAddIngredient = () => {
+    setBurgerData({
+      ...burgerData,
+      ingredients: [...burgerData.ingredients, ""],
+    });
+  };
+
+  const handleRemoveIngredient = (index) => {
+    const newIngredients = [...burgerData.ingredients];
+    newIngredients.splice(index, 1);
+    setBurgerData({ ...burgerData, ingredients: newIngredients });
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     addBurger(burgerData);
 
     setBurgerData({
       name: "",
-      ingredients: "",
+      ingredients: [],
       image: "",
       price: "",
     });
@@ -44,13 +63,28 @@ function AddBurgerForm({ addBurger }) {
         </Form.Group>
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
           <Form.Label>Ingredients</Form.Label>
-          <Form.Control
-            name="ingredients"
-            onChange={handleChange}
-            as="textarea"
-            rows={3}
-            value={burgerData.ingredients}
-          />
+          {burgerData.ingredients.map((ingredient, index) => (
+            <div key={index} className="mb-2">
+              <Form.Control
+                name="ingredients"
+                onChange={(e) => handleIngredientChange(e, index)}
+                type="text"
+                as="textarea"
+                value={ingredient}
+                rows={1}
+              />
+              <Button
+                variant="danger"
+                type="button"
+                onClick={() => handleRemoveIngredient(index)}
+              >
+                Remove Ingredient
+              </Button>
+            </div>
+          ))}
+          <Button variant="primary" type="button" onClick={handleAddIngredient}>
+            Add Ingredient
+          </Button>
         </Form.Group>
         <Form.Group controlId="formFile" className="mb-3">
           <Form.Label>Add Image</Form.Label>
